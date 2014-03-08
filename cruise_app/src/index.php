bc1b7b937e164ae885bc9c85046cd6e20155bb61
Richard <richard@touchlab.co.za>
2014-03-08 14:25:24 +0200

    added and tweaked global

--- cruise_app/src/index.php
+++ cruise_app/src/index.php
@@ -1,7 +1,6 @@
 <?php
-
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../../framework/yii.php';
+$global=dirname(__FILE__).'/global.php';
+$yii=dirname(__FILE__).'/protected/packages/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
