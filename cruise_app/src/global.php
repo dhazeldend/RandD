31cda56d654a781445aad8c2f10a8b7f18c3aaa5
Richard <richard@touchlab.co.za>
2014-03-08 14:56:09 +0200

    path fixes

--- cruise_app/src/global.php
+++ cruise_app/src/global.php
@@ -75,7 +75,7 @@ function asset($url=null)
 {
     static $baseUrl;
     if ($baseUrl===null)
-        $baseUrl=Yii::app()->getRequest()->getBaseUrl();
+        $baseUrl=Yii::app()->getRequest()->getBaseUrl() . "/assets";
     return $url===null ? $baseUrl : $baseUrl.'/'.ltrim($url,'/');
 }
  
--- cruise_app/src/index.php
+++ cruise_app/src/index.php
@@ -1,10 +1,13 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $global=dirname(__FILE__).'/global.php';
-$yii=dirname(__FILE__).'/protected/packages/framework/yii.php';
+$yii=dirname(__FILE__).'/protected/packages/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
+require_once($global);
 Yii::createWebApplication($config)->run();
