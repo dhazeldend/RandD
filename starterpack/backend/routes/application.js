19775bb3b450c4cca05bfcd27be62df7dc9de4c1
Richard <richard@touchlab.co.za>
2014-03-14 13:02:40 +0200

    added drawly and starterPackJS

---
+++ starterpack/backend/routes/application.js
@@ -0,0 +1,119 @@
+/*
+ * Jade is awesome, pass whatever data you want into the view. Just like that. 
+ * Current setup
+ * 
+ * nav_class = given class to add to nav-bar for custom styles, etc
+     nav_class: 'navbar-home',
+ * 
+ * nav_links = array of links to appear in the navigation bar with title and href
+
+    nav_links: [
+      { title: 'Home', href: '/home'},
+      { title: 'Setup', href: '/setup'},
+      { title: 'Update User', href: '/user/update'},
+      { title: 'Logout', href: '/logout'}
+    ]
+ * 
+ * user = if you need to pass in the current user to the partial
+       user: req.user
+ *
+ * coffeescript = array of javascript to pass into a given view
+      coffeescript: [
+        {js: '/js/postHelper.js'}
+      ],
+  * 
+  * styles = array of styles to pass into a given view
+        styles: [
+        {css: '/css/somefile.css'}
+      ],
+  *
+  * hide_navbar = boolean to hide or show navbar - false by default
+        hide_navbar:true
+ */
+ 
+exports.home = function(req, res) {
+	res.render('home', {
+    title: 'Home', 
+    nav_class: 'navbar-home',
+    nav_links: [
+      { title: 'Home', href: '/home'},
+    	{ title: 'Setup', href: '/setup'},
+    	{ title: 'Update User', href: '/user/update'},
+      { title: 'GitHub Repo', href: 'http://www.github.com/nickalto/starterpack.js'},
+		  { title: 'Logout', href: '/logout'}
+    ]
+  });
+};
+
+exports.setup = function(req, res) {
+	res.render('setup', {
+    title: 'Setup', 
+    nav_class: 'navbar-setup',
+    nav_links: [
+    	{ title: 'Home', href: '/home'},
+      { title: 'Setup', href: '/setup'},
+    	{ title: 'Update User', href: '/user/update'},
+      { title: 'GitHub Repo', href: 'http://www.github.com/nickalto/starterpack.js'},
+		  { title: 'Logout', href: '/logout'}
+    ]
+  });
+};
+
+exports.login = function(req, res) {
+    console.log("hi");
+    res.render('login', {
+    	title: 'Login', 
+      nav_class: 'navbar-login',
+    	nav_links: [
+        { title: 'Home', href: '/home'},
+    		{ title: 'Create User', href: '/create'},
+        { title: 'GitHub Repo', href: 'http://www.github.com/nickalto/starterpack.js'},
+    	],
+      user: req.user
+  	});
+};
+
+exports.createUser = function(req, res) {
+	res.render('create', {
+    	title: 'Create User', 
+	    coffeescript: [
+	    	{js: '/js/postHelper.js'}
+	    ],
+    	nav_links: [
+    		{ title: 'Login', href: '/login'},
+        { title: 'GitHub Repo', href: 'http://www.github.com/nickalto/starterpack.js'},
+    	]
+  	});
+};
+
+exports.updateUser = function(req, res) {
+  res.render('update', {
+      title: 'Update User', 
+      coffeescript: [
+        {js: '/js/postHelper.js'}
+      ],
+      nav_links: [
+        { title: 'Home', href: '/home'},
+        { title: 'Setup', href: '/setup'},
+        { title: 'Update User', href: '/user/update'},
+        { title: 'GitHub Repo', href: 'http://www.github.com/nickalto/starterpack.js'},
+        { title: 'Logout', href: '/logout'}      
+      ],
+      user: req.user
+    });
+};
+
+exports.logout = function(req, res) {
+	req.logout();
+	res.redirect('/');
+};
+
+exports.catchall = function(req, res) {
+	res.render('404', {
+    	title: '404', 
+	    hide_navbar:true
+  	});
+};
+
+
+
---
+++ starterpack/router.js
@@ -0,0 +1,46 @@
+var express	= require('express'),
+app 		= express(),
+config 		= require('./config/config').configure(app),
+https	    = require('https'),
+http        = require('http'),
+app_routes 	= require('./backend/routes/application'),
+auth_routes	= require('./backend/routes/authentication'),
+base_functions = require('./backend/passport/base');
+
+
+app.get('(/|/login)', app_routes.login);
+app.get('/home', auth_routes.isAuthenticated, app_routes.home);
+app.get('/setup', auth_routes.isAuthenticated, app_routes.setup);
+app.get('/create', app_routes.createUser);
+app.get('/user/update', auth_routes.isAuthenticated, app_routes.updateUser);
+app.get('/logout', app_routes.logout);
+
+//local authentication routes
+app.post('/user/create', auth_routes.localCreate);	
+app.post('/user/delete', auth_routes.isAuthenticated, auth_routes.localDelete);
+app.post('/user/update', auth_routes.isAuthenticated, auth_routes.localUpdate);
+app.post('/user/password', auth_routes.isAuthenticated, auth_routes.localPasswordUpdate);
+app.post('/login', auth_routes.localAuthentication);
+
+//social authentication routes
+app.get('/auth/google', auth_routes.googleAuthentication);
+app.get('/google/unlink', auth_routes.isAuthenticated, auth_routes.googleUnlink);
+app.get('/auth/google/callback', auth_routes.googleCallback);
+app.get('/auth/twitter', auth_routes.twitterAuthentication);
+app.get('/twitter/unlink', auth_routes.isAuthenticated, auth_routes.twitterUnlink);
+app.get('/auth/twitter/callback', auth_routes.twitterCallback);
+app.get('/auth/facebook', auth_routes.facebookAuthentication);
+app.get('/facebook/unlink', auth_routes.isAuthenticated, auth_routes.facebookUnlink);
+app.get('/auth/facebook/callback', auth_routes.facebookCallback);
+app.get('/auth/github', auth_routes.githubAuthentication);
+app.get('/github/unlink', auth_routes.isAuthenticated, auth_routes.githubUnlink);
+app.get('/auth/github/callback', auth_routes.githubCallback);
+
+// 404 catchall
+app.use( app_routes.catchall);
+
+http.createServer(app).listen(3000);
+
+/* //Uncomment for SSL
+https.createServer(app.ssl, app).listen(3001);
+*/
